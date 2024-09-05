6e1c59c09df48096cb6467fd737ff8191013f4de
Keith Schacht <krschacht@gmail.com>
2024-09-05 09:22:11 -0500

    cleanup backticks

--- lib/system/backticks.rb
+++ lib/system/backticks.rb
@@ -1,16 +1,3 @@
-class String
-  alias_method :original_inspect, :inspect
-
-  def inspect
-    # if self =~ /["`]/
-    #   "`#{self}`"
-    # else
-    #   original_inspect
-    # end
-    "`#{self}`"
-  end
-end
-
 module Kernel
   alias_method :original_backticks, :`
 
@@ -20,5 +7,13 @@ module Kernel
 
     # Evaluate the string within the current context
     eval("'#{str}'", binding, __FILE__, __LINE__)
+  end
+end
+
+class String
+  alias_method :original_inspect, :inspect
+
+  def inspect
+    "`#{self}`"
   end
 end
